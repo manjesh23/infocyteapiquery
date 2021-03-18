56fee46f0fc1ab3f5a79f7683714ed0d4b387bca
Manjesh Setty <manjesh_n@hotmail.com>
2021-03-18 13:03:50 +0530

    commit

--- setup.py
+++ setup.py
@@ -26,8 +26,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
