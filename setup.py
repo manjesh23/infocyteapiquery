654a457b2a4f7564302093de0474b9954b96bc79
Manjesh Setty <manjesh_n@hotmail.com>
2021-04-11 21:42:36 +0530

    setup file update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.md') as history_file:
     history = history_file.read()
 
-requirements = ["requests", "pandas", "tqdm", "subprocess", "paginateit"]
+requirements = ["requests", "pandas", "tqdm", "paginateit"]
 
 # setup_requirements = ['pytest-runner', ]
 
