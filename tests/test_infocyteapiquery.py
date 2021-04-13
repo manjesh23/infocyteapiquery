782eb3306d314bcc4af197b57601a62aee0e86c7
Manjesh Setty <manjesh_n@hotmail.com>
2021-04-13 08:48:13 +0530

    Update test_infocyteapiquery.py

--- tests/test_infocyteapiquery.py
+++ tests/test_infocyteapiquery.py
@@ -7,21 +7,16 @@ import pytest
 # Import required modules
 from infocyteapiquery import infocyteapiquery as ic
 import os
-from dotenv import load_dotenv, find_dotenv
+#from dotenv import load_dotenv, find_dotenv
 from paginateit import paginateit as pg
 
 # Py_test start
 
-#load_dotenv(find_dotenv())
-
-#cname = os.environ.get("CNAME")
-#apikey = os.environ.get("APIKEY")
 apiquery = "AlertDetails"
 
 
 def test_query():
-    #load_dotenv(find_dotenv("manjesh.env"))
-    #cname = os.getenv("cname")
-    #apikey = os.getenv("apikey")
+    cname = os.getenv("cname")
+    apikey = os.getenv("apikey")
     ic.query(cname, apikey, apiquery)
     assert ic.icd.status_code == 200
