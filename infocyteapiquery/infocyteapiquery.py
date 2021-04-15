ee244f3e1a879ddf2944b773360398539058129a
Manjesh Setty <manjesh_n@hotmail.com>
2021-04-15 22:40:07 +0530

    PowerShell Bug fix

--- infocyteapiquery/infocyteapiquery.py
+++ infocyteapiquery/infocyteapiquery.py
@@ -72,7 +72,8 @@ def ps(cname="cname", apikey="apikey", pscmd="pscmd"):
     for line in tqdm(pscmd.splitlines(), desc="Loading ", ncols=100, unit='Line(s)', bar_format='{l_bar}{bar} | {n_fmt}/{total_fmt} {unit}', colour='BLUE'):
         raw = subprocess.run(
             ["powershell.exe", "-Command", key + line], capture_output=True)
-        outcome = raw.stdout.decode("utf-8")[6:]
+        outcome = raw.stdout.decode("utf-8")
+        outcome = re.sub('(True)\r\n', '', outcome)
     return(outcome)
 
 
@@ -81,18 +82,15 @@ This is PowerShell base64-Encoded Funcation
 '''
 
 
-def pse(cname="cname", apikey="apikey", psecmd="psecmd"):
+def ps(cname="cname", apikey="apikey", pscmd="pscmd"):
     key = "Set-ICToken -Instance " + cname + " -Token " + \
         apikey + ";Set-ICBox -Global -Last 90;"
-    psecmd = psecmd.replace('\n', ';')
-    etext = ("$pt=\'" + key+psecmd +
-             "\';$enc = [convert]::ToBase64String([System.Text.encoding]::Unicode.GetBytes($pt));$enc")
-    for line in tqdm(etext.splitlines(), desc="Loading ", ncols=100, unit='Line(s)', bar_format='{l_bar}{bar} | {n_fmt}/{total_fmt} {unit}', colour='BLUE'):
-        raw = subprocess.run(["powershell.exe", line], capture_output=True)
-        eoutcome = raw.stdout.decode("utf-8")
-        data = subprocess.run(
-            ["powershell.exe", "-encoded", eoutcome], capture_output=True)
-        output = data.stdout.decode("utf-8")[6:]
-    return(output)
+    pscmd = pscmd.replace('\n', ';')
+    for line in tqdm(pscmd.splitlines(), desc="Loading ", ncols=100, unit='Line(s)', bar_format='{l_bar}{bar} | {n_fmt}/{total_fmt} {unit}', colour='BLUE'):
+        raw = subprocess.run(
+            ["powershell.exe", "-Command", key + line], capture_output=True)
+        outcome = raw.stdout.decode("utf-8")
+        outcome = re.sub('(True)\r\n', '', outcome)
+    return(outcome)
 
 # EOF
