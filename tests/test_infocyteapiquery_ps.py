27c7781acc83a1c9affd6a897cbc5ede2d125d2e
Manjesh Setty <manjesh_n@hotmail.com>
2021-04-20 01:27:17 +0530

    ps pytest

--- tests/test_infocyteapiquery_ps.py
+++ tests/test_infocyteapiquery_ps.py
@@ -16,7 +16,6 @@ def test_ps():
     cname = os.getenv("cname")
     apikey = os.getenv("apikey")
     pscmd = "$psvariable = Get-ICAlert\n($psvariable).name\n"
-    pstest = ic.ps(cname, apikey, pscmd)
-    print(pstest)
+    assert bool(ic.ps(cname, apikey, pscmd)) == True
     #assert re.search('exe|dll', pstest)
     #assert bool(pstest) == True
