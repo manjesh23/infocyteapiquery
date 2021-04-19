d3d5006986de2a11b8f5c86d827359a48dc30207
Manjesh Setty <manjesh_n@hotmail.com>
2021-04-20 01:08:08 +0530

    pytest

--- tests/test_infocyteapiquery_ps.py
+++ tests/test_infocyteapiquery_ps.py
@@ -11,14 +11,13 @@ import re
 
 
 # Py_test start
-pscmd = "$psvariable = Get-ICAlert\n($psvariable).name\n"
-
 
 def test_ps():
     #cname = os.getenv("cname")
     #apikey = os.getenv("apikey")
     cname = "manjeshlan"
     apikey = "TvozHxIjZwGxDzj1aoivDRT33wEfVWCsFLraqJUPnpp3fDkAOHjwYDYJK2BLqNd5"
+    pscmd = "$psvariable = Get-ICAlert\n($psvariable).name\n"
     pstest = ic.ps(cname, apikey, pscmd)
     #assert re.search('exe|dll', pstest)
     assert 'dll' in str(pstest)
