75e0814e30858193201bf23f4d3e659569efd70a
Manjesh Setty <manjesh_n@hotmail.com>
2021-05-03 20:59:04 +0530

    yaml parser

--- docs/js/script.js
+++ docs/js/script.js
@@ -8,3 +8,25 @@ function display_ct() {
     document.getElementById('ct').innerHTML = x;
     display_c();
 }
+
+function opentab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function regexmatch() {
+    var yamlInput = document.getElementById("yamlInput").value;
+    var dataInput = document.getElementById("dataInput").value;
+    var result = yamlInput.match(dataInput);
+    document.getElementById("demo").innerHTML = result;
+
+}
